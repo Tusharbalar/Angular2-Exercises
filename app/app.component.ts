d8b4bde31620712910030766aaa371d4da3c3d85
TusharBalar <balar.tushar1@gmail.com>
2016-10-20 00:03:55 +0530

    Embedded styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
     template: `
         <h1>My First Angular App</h1>
         <fa-other></fa-other>
-    `
+    `,
+    styles: [`
+        h1 {
+            color: red
+        }
+    `]
 })
 export class AppComponent { }
