25e51156f923846d18a9891b992a1595a45a91f3
TusharBalar <balar.tushar1@gmail.com>
2016-10-20 01:14:44 +0530

    Property Binding

--- app/other/other.component.ts
+++ app/other/other.component.ts
@@ -2,11 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'fa-other',
-  templateUrl: './app/other/other.component.html'
+  templateUrl: './app/other/other.component.html',
+  styleUrls: ['./app/other/other.component.css']
 })
 
 export class OtherComponent {
 
   stringInterpolation = "This is string interpolation";
   numberInterpolation = 2;
+
+  onTest() {
+    return true;
+  }
 }
