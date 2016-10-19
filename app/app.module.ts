910426d61298fb8815febb746237986e89fefd22
TusharBalar <balar.tushar1@gmail.com>
2016-10-20 00:13:04 +0530

    Create one more component

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { OtherComponent }  from './other/other.component';
+import { AnotherComponent }  from './other/another.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent, OtherComponent ],
+  declarations: [ AppComponent, OtherComponent,AnotherComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/other/another.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fa-another',
+  template: '<h2>Another Component</h2>'
+})
+
+export class AnotherComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+
+}
