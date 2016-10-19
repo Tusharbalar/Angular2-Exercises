aa4c6c379b6e2a485c5ec0bdfc3a133c7e80db09
TusharBalar <balar.tushar1@gmail.com>
2016-10-20 00:01:18 +0530

    Create new component

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `
         <h1>My First Angular App</h1>
+        <fa-other></fa-other>
     `
 })
 export class AppComponent { }
---
+++ app/other/other.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fa-other',
+  templateUrl: './app/other/other.component.html'
+})
+
+export class OtherComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+
+}
