127568e864ebeb22d362d65a430bcbfd40132da8
TusharBalar <balar.tushar1@gmail.com>
2016-10-20 09:33:31 +0530

    Binding to Custom Properties

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { OtherComponent }  from './other/other.component';
 import { AnotherComponent }  from './other/another.component';
+import { PropertyBindingComponent }  from './other/property-binding.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent, OtherComponent,AnotherComponent ],
+  declarations: [ AppComponent, OtherComponent,AnotherComponent, PropertyBindingComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
